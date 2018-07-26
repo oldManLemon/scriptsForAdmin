0e342687c78a1cdc8f37849dcf0e26a05f5797e0
hase <oldm8reggie@gmail.com>
2018-07-26 10:43:05 +1000

    can select correct jobs with small input menu

--- mailArchiver/mover.py
+++ mailArchiver/mover.py
@@ -1,19 +1,39 @@
 import os
 
+
+def intCheck(number):
+    try:
+        return int(number)
+    except ValueError:
+        return False
+
+
 """ First Get list of Files available for moving and print"""
 """ We are going to use binary to scan for file names """
 jobsList = os.listdir("example/binary")
 # print(jobsList)
-for jobs in enumerate(jobsList):
+for jobs in enumerate(jobsList, start=1):
     print(jobs)
 
 """ Then allow selection of available files"""
- actionJobs = raw_input("Please select files, seperate with comma: Eg: 2,4 \n")
+actionJobs = input("Please select files, seperate with comma: Eg: 2,4 \n")
+actionArray = []
 
-for actions in range(int(actionJobs)):
-    print(actions)
+for actions in actionJobs:
+    # print(actions)
+    if intCheck(actions) == int():
+        print("")
+    else: 
+            # print(actions)
+            actionArray.append(int(actions))
+  
+print(actionArray)
+
+""" Select jobs """
+for  x in actionArray:
+    print(jobsList[x-1])
+
 
 """ Then detach database if posssible """
 
 """ Then move files acordingly """
-
